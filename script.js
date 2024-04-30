a292b4aa58e6cb2d5cef3c394b8058db86413dbc
bekov719 <odilbekhaitkulov@gmail.com>
2024-04-30 22:23:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Sizning typedagi malumot kiriting")
+let num = prompt("Number typedagi malumot kiriting")
+let nu = prompt("Boolean typedagi malumot kiriting")
+
+alert('hamma malumotlarni togri kiritganizga ishonchingiz komilmi?')
+alert('Siz kiritgan ' + num1 +  ' sozi stringa tegishli ' + 'Siz kiritgan ' + num + ' sozi numberga tegishli.' +
+' Siz kiritgan ' + nu + ' sozi boolerga tegishli.')
+
+.console.log(num1 - num2);         
